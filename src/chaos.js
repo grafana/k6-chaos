7324166db7c5d66cce47a786cf77aa6ea58b2884
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2022-04-26 15:16:36 +0200

    Add node-based stress helpers
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- src/chaos.js
+++ src/chaos.js
@@ -9,39 +9,78 @@ export class KubernetesClient {
         }
     }
 
-    getPodNames(nameSpace) {
-        return this.kubernetes.pods.list(nameSpace).map(function(pod){
+    getPodNames(namespace) {
+        return this.kubernetes.pods.list(namespace).map(function(pod){
             return pod.name
         })
     }
 
-    getJobNames(nameSpace) {
-        return this.kubernetes.jobs.list(nameSpace).map(function(job){
+    getJobNames(namespace) {
+        return this.kubernetes.jobs.list(namespace).map(function(job){
             return job.name
         })
     }
 
-    getRandomPod(nameSpace) {
-        const podNames = this.getPodNames(nameSpace)
+    getNodeNames() {
+        return this.kubernetes.nodes.list().map(function(node){
+            return node.name
+        })
+    }
+
+    getRandomPod(namespace) {
+        const podNames = this.getPodNames(namespace)
         return podNames[Math.floor(Math.random() * podNames.length)]
     }
 
-    getRandomJob(nameSpace) {
-        const jobNames = this.getJobNames(nameSpace)
+    getRandomJob(namespace) {
+        const jobNames = this.getJobNames(namespace)
         return jobNames[Math.floor(Math.random() * jobNames.length)]
     }
 
+    runJobOnNodes(namespace, jobName, nodes, image, command){
+        for (let i = 0; i < nodes.length; i++) {
+          let name = `${jobName}-${(Math.random() + 1).toString(36).substring(7)}`;
+          console.log(`Creating job ${name} on node ${nodes[i]}`)
+          this.kubernetes.jobs.create({
+            namespace: namespace,
+            name: name,
+            image: image,
+            command: command,
+            node_name: nodes[i]
+          })
+        }
+        return jobName
+    }
+
+    cleanJobs(namespace, jobName){
+        let jobs = this.getJobNames(namespace)
+        for (let i = 0; i < jobs.length; i++) {
+            if (jobs[i].includes(jobName)) {
+                console.log(`Deleting job ${jobs[i]}`)
+                this.kubernetes.jobs.delete(jobs[i],namespace)
+            }
+        }
+        let pods = this.getPodNames(namespace)
+        for (let i = 0; i < pods.length; i++) {
+            if (pods[i].includes(jobName)) {
+                console.log(`Deleting pod ${pods[i]}`)
+                this.kubernetes.pods.delete(pods[i],namespace)
+            }
+        }
+        return jobName
+    }
+
     // Kill a random pod in the specified namespace.
-    killRandomPod(nameSpace) {
-        const podName = this.getRandomPod(nameSpace)
-        this.kubernetes.pods.kill(podName,nameSpace)
+    killRandomPod(namespace) {
+        const podName = this.getRandomPod(namespace)
+        this.kubernetes.pods.kill(podName,namespace)
         return podName
     }
 
     // Kill a random job in the specified namespace.
-    killRandomJob(nameSpace) {
-        const jobName = this.getRandomJob(nameSpace)
-        this.kubernetes.jobs.kill(jobName,nameSpace)
+    killRandomJob(namespace) {
+        const jobName = this.getRandomJob(namespace)
+        this.kubernetes.jobs.kill(jobName,namespace)
         return jobName
     }
 
@@ -51,5 +90,65 @@ export class KubernetesClient {
         const randomNamespace = namespaces[Math.floor(Math.random() * namespaces.length)]
         this.kubernetes.namespaces.delete(randomNamespace.name)
         return randomNamespace.name
+    }
+}
+
+export class StressNodesAttack {
+    constructor(client) {
+        this.kubernetes = client
+        this.name = 'stress-nodes'
+        this.namespace = 'default'
+        this.cpu = '0'
+        this.io = '0'
+        this.vm = '0'
+        this.vmBytes = '256MB'
+        this.timeout = '10'
+    }
+
+    inNamespace(namespace) {
+        this.namespace = namespace
+        return this
+    }
+    inNodes(nodes) {
+        this.nodes = nodes
+        return this
+    }
+    withName(name) {
+        this.name = name
+        return this
+    }
+    withCPU(cpu) {
+        this.cpu = cpu
+        return this
+    }
+    withIO(io) {
+        this.io = io
+        return this
+    }
+    withVM(vm) {
+        this.vm = vm
+        return this
+    }
+    withVMBytes(vmBytes) {
+        this.vmBytes = vmBytes
+        return this
+    }
+    withTimeout(timeout) {
+        this.timeout = timeout
+        return this
+    }
+    start() {
+        this.kubernetes.runJobOnNodes(
+            this.namespace,
+            this.name,
+            this.nodes,
+            'progrium/stress',
+            ['stress', '--cpu', this.cpu, '--io', this.io, '--vm', this.vm, '--vm-bytes', this.vmBytes, '--timeout', this.timeout]
+        )
+        return this.name
+    }
+    clean() {
+        this.kubernetes.cleanJobs(this.namespace, this.name)
+        return this.name
     }
 }
